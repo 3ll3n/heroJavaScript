087c6e77e331b24a5a0b7c09492e9960169828aa
3ll3n <ellengrafton01@gmail.com>
2017-03-22 20:12:57 +0000

    created touch test/method for rat

--- rat.js
+++ rat.js
@@ -2,10 +2,10 @@ var Rat = function(name) {
   this.name = name;
 };
 
-// Rat.prototype = {
-//   poison: function() {
-
-//   }
-// };
+Rat.prototype = {
+  touch: function(food) {
+    return this.name + " touched the " + food.name + "!";
+  }
+};
 
 module.exports = Rat;
--- specs/rat_spec.js
+++ specs/rat_spec.js
@@ -1,20 +1,23 @@
 var Rat = require('../rat');
 var assert = require('assert');
+var Food = require('../food');
 
 describe('Rat', function() {
 
   var rat;
+  var food;
 
   beforeEach(function() {
     rat = new Rat('Hordak');
+    food = new Food('steak', 10);
   });
 
   it('should have a name', function() {
     assert.equal('Hordak', rat.name);
   });
 
-  // it('should be able to poison touched food', function() {
-
-  // });
+  it('should be able to touch food', function() {
+    assert.equal('Hordak touched the steak!', rat.touch(food));
+  });
 
 });
